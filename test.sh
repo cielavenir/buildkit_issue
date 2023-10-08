628ee4efb8ebce6c3baab999aa56568bb227e625
cielavenir <cielartisan@gmail.com>
2023-10-08 15:54:42 +0900

    continue

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ source namespace.conf
 if ! sudo `which nerdctl` --namespace=${NAMESPACE} inspect alpine:latest >/dev/null; then
 sudo `which nerdctl` --namespace=${NAMESPACE} pull alpine:latest
 fi
-sudo `which nerdctl` --namespace=${NAMESPACE} tag alpine:latest alpine:nonexisting
-sudo `which nerdctl` --namespace=${NAMESPACE} build -t alpine:test test
+sudo `which nerdctl` --namespace=${NAMESPACE} build -t alpine:nonexisting1 test1
+sudo `which nerdctl` --namespace=${NAMESPACE} build -t alpine:nonexisting2 test2
